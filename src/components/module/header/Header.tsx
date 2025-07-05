58634f1e3024524aa67cc8a3dc341b541dfe81f0
Shakil Ahmed <shakilahmed5161@gmail.com>
2025-07-05 22:28:21 +0600

    update fav icon link

--- src/components/module/header/Header.tsx
+++ src/components/module/header/Header.tsx
@@ -9,7 +9,7 @@ function Header() {
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="../../../../favicon.ico" className="h-8" alt="Book Cart Logo" />
+                <img src="https://raw.githubusercontent.com/Shakil5161/ph_library_management_system/refs/heads/main/favicon.ico" className="h-8" alt="Book Cart Logo" />
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">BookCart</span>
             </Link>
             <button onClick={()=> setIsOpen(!isOpen)} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
